ab5580dfbd1ac4094e0fdb3f3be5c01b3a7562a2
Hermann Azong <hermann.azong@googlemail.com>
2016-03-23 12:12:33 +0100

    add new packages for c.s.hortsmann book

---
+++ src/de/hermann/c/s/horstmann/chapter5/Manager.java
@@ -0,0 +1,29 @@
+package de.hermann.c.s.horstmann.chapter5;
+
+public class Manager extends Employee {
+
+	private double bonus;
+
+	/**
+	 * 
+	 * @param name
+	 * @param salary
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public Manager(String name, double salary, int year, int month, int day) {
+		super(name, salary, year, month, day);
+		bonus = 0;
+	}
+
+	public double getSalary() {
+		double baseSalary = super.getSalary();
+		return baseSalary + bonus;
+	}
+
+	public void setBonus(double b) {
+		bonus = b;
+	}
+
+}
